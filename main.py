32ecc0be20d62c52f8ee12ecefd1c8d278ad6d39
unknown <24mreborn1@gmail.com>
2023-11-15 17:39:30 +0800

    perubaha pada ui

--- main.py
+++ main.py
@@ -1,43 +1,63 @@
 from PyQt6.QtWidgets import * 
 from PyQt6.QtGui import *
-from PyQt6.QtCore import Qt
+from PyQt6.QtSvg import *
+from PyQt6.QtCore import *
 import sys
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Awanara")
-        container = QFrame()
-        layout = QGridLayout(container)
-        pagelayout = QHBoxLayout(container)
-        sidelayout = QVBoxLayout(container)
-        self.setStyleSheet('background-color: #202A44')
-        self.setBaseSize(900, 480)
-        container.setStyleSheet('background-color: white')
-        button1 = QPushButton("Home")
-        button2 = QPushButton("List")
-        button3 = QPushButton("About")
-        judul = QLabel("AWANARA")
+        self.setWindowTitle("Awanari")
+        layout = QGridLayout()
+        pagelayout = QHBoxLayout()
+        sidelayout = QVBoxLayout()
+        self.setStyleSheet('background-color: #52CEBA')
+        self.setMinimumSize(800,550)
+        button1 = QPushButton("")
+        button2 = QPushButton("")
+        button3 = QPushButton("")
         label1 = QLabel("Test")
         label2 = QLabel("Test2")
+        label3 = QLabel("Test3")
         button1.setMaximumSize(1000,1000)
+        button1.setIcon(QIcon('./asset/svg/home.svg'))
+        button2.setIcon(QIcon('./asset/svg/search.svg'))
+        
         button2.setMaximumSize(1000,1000)
         button3.setMaximumSize(1000,1000)
-        sidelayout.addWidget(judul)
         sidelayout.addWidget(button1)
         sidelayout.addWidget(button2)
         sidelayout.addWidget(button3)
         pagelayout.addWidget(label1)
         pagelayout.addWidget(label2)
-        layout.addLayout(sidelayout,0,0)
-        layout.addLayout(pagelayout,0,1,1,9)
+        pagelayout.addWidget(label3)
+        layout.addLayout(sidelayout,0,0,0,0)
+        layout.addLayout(pagelayout,0,1,1,3)
         self.setLayout(layout)
         for label in self.findChildren(QLabel):
             label.setStyleSheet('color: black;padding:30px;border-radius:15px;font-weight:bold;font-size:35px;color:white;')
         for button in self.findChildren(QPushButton):
-            button.setStyleSheet('background: #83A2FF;color: white;padding:10px;margin:10px;height:auto;border-radius:13px;font-weight:bold;font-size:30px;')
-            button.setMinimumHeight(80)
-        
+            button.setStyleSheet('''
+                                 .QPushButton{
+                                 background: white;
+                                 color: white;
+                                 margin:5px;
+                                 height:auto;
+                                 border-radius:13px;
+                                 font-weight:bold;
+                                 font-size:20px;
+                                 }
+                                 .QPushButton:hover{
+                                 background: #FFA33C;
+                    
+                                 }
+                                 ''')
+            
+            button.setMinimumHeight(120)
+            button.setMinimumWidth(30)
+            button.setMaximumHeight(240)
+            button.setMaximumWidth(90)
+            button.setIconSize(QSize(50,50))
 app = QApplication([])
 window = Window()
 window.show()
